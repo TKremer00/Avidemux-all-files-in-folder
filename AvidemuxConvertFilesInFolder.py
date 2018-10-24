02c254c955ff34fb7c0fa5533bea0b5b24aea5d8
TKremer00 <38754668+TKremer00@users.noreply.github.com>
2018-10-24 15:22:44 +0200

    Update AvidemuxConvertFilesInFolder.py

--- AvidemuxConvertFilesInFolder.py
+++ AvidemuxConvertFilesInFolder.py
@@ -6,14 +6,16 @@ adm = Avidemux()
 ext = "mp4"
 sep = "\\"
 
+#Custom name ? replace None
+filename = None
 
 #always let the user first select source folder 
 inputFolder = gui.dirSelect("Select the source folder", "test")
 
 #apply filters
-def convert(filein):   
-  filename = basename(filein)
-  dir = dirname(filein)
+def convert(filein):  
+  if(filename is None):
+	filename = basename(filein)
   
   if(0 == adm.loadVideo(filein)):
       ui.displayError("oops","cannot load "+filein)
